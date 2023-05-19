4e362cd4ec5283b25e2af2c155a554a20da631c9
Carlos Vinicius <viniciusdev.26@gmail.com>
2023-05-19 20:25:40 -0300

    feat: create nest validation

--- src/account/controllers/account-controller.ts
+++ src/account/controllers/account-controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateAccountService } from '../services/create-account.service';
+import { CreateAccountDTO } from '../dtos/create-account.dto';
 
 @Controller('/account')
 export class AccountController {
   constructor(private readonly createAccountService: CreateAccountService) {}
 
   @Post('/')
-  async createAccount(@Body() params) {
+  async createAccount(@Body() params: CreateAccountDTO) {
     await this.createAccountService.execute(params);
   }
 }
