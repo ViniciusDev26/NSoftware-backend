66e88cd97d2e54ded7c432ac7915578c7197a18e
Carlos Vinicius <viniciusdev.26@gmail.com>
2023-05-19 20:52:41 -0300

    feat: create swagger config and compodoc config

--- src/account/dtos/create-account.dto.ts
+++ src/account/dtos/create-account.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export abstract class CreateAccountDTO {
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   password: string;
 }
